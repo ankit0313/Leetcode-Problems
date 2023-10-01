e820a52aab2d1bb8c0a6c17c437da0b01fea90a1
Liladhar Varma <liladharvarm29@gmail.com>
2023-10-01 13:44:27 +0530

    added comments

--- Leetcode/JewelsAndStones.java
+++ Leetcode/JewelsAndStones.java
@@ -18,12 +18,17 @@ class Solution {
 //Another Solution with Time Complexity of O(n + m)  and Space Complexity of O(n) 
 
      public int numberOfStones(String jewels , String stones){
-        int totalStones = 0 ;
+        int totalStones = 0 ;  // to keep track of stones 
+			       
+//Using hasmap to keep strack of stones and their occurences
         Map<Character,Integer> freqMap = new HashMap<Character , Integer>();
 
+	//puuting stones into map with freq of occurence 
         for(char c : stones.toCharArray()){
             freqMap.put(c , freqMap.getOrDefault(c , 0) + 1);
         }
+
+	//Checking wether map contains the key or not if yes then adding count 
 
         for(char ch : jewels.toCharArray()){
             int value = 0 ;
