5ba2d56d6b5a501106dba4ccd4b4d88ee3713902
Liladhar Varma <liladharvarm29@gmail.com>
2023-10-01 12:14:00 +0530

    Solution with O(n) Time and Space Complexity

--- Leetcode/SquaresOfSortedArray.java
+++ Leetcode/SquaresOfSortedArray.java
@@ -18,5 +18,33 @@ class Solution {
             nums[k+1] = key;
         }
         return nums;
+}
+
+
+
+//Another Solution with Time And Sppace Complexity of O(n)
+public int[] sortedSquares(int[] nums) {
+        int n = nums.length;
+    int[] result = new int[n];
+    int left = 0;
+    int right = n - 1;
+    int idx = n - 1;
+
+    while (left <= right) {
+        int leftSquare = nums[left] * nums[left];
+        int rightSquare = nums[right] * nums[right];
+
+        if (leftSquare > rightSquare) {
+            result[idx] = leftSquare;
+            left++;
+        } else {
+            result[idx] = rightSquare;
+            right--;
+        }
+
+        idx--;
+    }
+
+    return result;
     }
 }
